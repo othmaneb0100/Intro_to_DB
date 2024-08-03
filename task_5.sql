463289b7876d5767009d1c47b025accbfb628401
othmaneb0100 <othmaneboussaid0100@gmail.com>
2024-08-03 14:38:19 +0000

    li

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,6 @@
 -- Select the database
 USE alx_book_store;
 
--- Insert multiple rows into the customer table
+-- Insert a single row into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
